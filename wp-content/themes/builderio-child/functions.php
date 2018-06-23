e9150675e63c059b43b4076a8109dbfac3806536
Bartek B. <bartek.bugala@gmail.com>
2018-06-23 16:48:30 +0200

    Builderio Child - NOWA SKÓRKA

---
+++ wp-content/themes/builderio-child/functions.php
@@ -0,0 +1,52 @@
+<?php
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+function my_theme_enqueue_styles() {
+    wp_enqueue_style( 'builderio', get_template_directory_uri() . '/style.css' );
+
+}
+
+/* Bartek - Dodanie kolumny Size do Media Library - sprawdzone: działa */
+
+add_filter( 'manage_media_columns', 'sk_media_columns_filesize' );
+/**
+ * Filter the Media list table columns to add a File Size column.
+ *
+ * @param array $posts_columns Existing array of columns displayed in the Media list table.
+ * @return array Amended array of columns to be displayed in the Media list table.
+ */
+function sk_media_columns_filesize( $posts_columns ) {
+	$posts_columns['filesize'] = __( 'File Size', 'my-theme-text-domain' );
+
+	return $posts_columns;
+}
+
+add_action( 'manage_media_custom_column', 'sk_media_custom_column_filesize', 10, 2 );
+/**
+ * Display File Size custom column in the Media list table.
+ *
+ * @param string $column_name Name of the custom column.
+ * @param int    $post_id Current Attachment ID.
+ */
+function sk_media_custom_column_filesize( $column_name, $post_id ) {
+	if ( 'filesize' !== $column_name ) {
+		return;
+	}
+
+	$bytes = filesize( get_attached_file( $post_id ) );
+
+	echo size_format( $bytes, 2 );
+}
+
+add_action( 'admin_print_styles-upload.php', 'sk_filesize_column_filesize' );
+/**
+ * Adjust File Size column on Media Library page in WP admin
+ */
+function sk_filesize_column_filesize() {
+	echo
+	'<style>
+		.fixed .column-filesize {
+			width: 10%;
+		}
+	</style>';
+}
+?>
