7411b8882d83652f95f89faaaf842b74f2b97b98
Bartek B. <bartek.bugala@gmail.com>
2018-06-27 00:41:27 +0200

    Google Analytics <script>   window.dataLayer = window.dataLayer || [];   function gtag(){dataLayer.push(arguments);}   gtag('js', new Date());
      gtag('config', 'UA-121407399-1');
    </script>

--- wp-content/themes/builderio-child/header.php
+++ wp-content/themes/builderio-child/header.php
@@ -13,6 +13,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="http://gmpg.org/xfn/11">
     <?php wp_head(); ?>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-121407399-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-121407399-1');
+</script>
 </head>
 
 <body <?php body_class(); ?>>
