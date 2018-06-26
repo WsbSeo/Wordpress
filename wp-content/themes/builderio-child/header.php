4f864c0a63df7cf75d2b152d6f327dee5f172f02
Bartek B. <bartek.bugala@gmail.com>
2018-06-27 00:14:52 +0200

    FACEBOOK JS

--- wp-content/themes/builderio-child/header.php
+++ wp-content/themes/builderio-child/header.php
@@ -16,6 +16,16 @@
 </head>
 
 <body <?php body_class(); ?>>
+<!--Start Facebook-->
+    <div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/pl_PL/sdk.js#xfbml=1&version=v3.0';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+<!--End Facebook-->
 <div id="page" class="site">
 <?php $builderio_pageloader_status = builderio_get_option( 'builderio_pageloader_status' );
 	  $builderio_pageloader_image  = builderio_get_option( 'builderio_pageloader_image' );
